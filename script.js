dd1d34f1d342bf30dda0d60fe0a59891d4f17c5c
softdac-web <softdac@gmail.com>
2026-03-01 22:47:25 -0300

    Refactor script.js to improve product handling

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Imagen principal
     let imagenPrincipal = document.createElement("img");
     imagenPrincipal.src = `img/${actual.codigo}_000.jpg`;
-    imagenPrincipal.style.width = "100%";
+    imagenPrincipal.style.width = "80%";
     imagenPrincipal.style.borderRadius = "12px";
     imagenPrincipal.style.marginBottom = "10px";
     galeriaDiv.appendChild(imagenPrincipal);
@@ -250,3 +250,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 });
+
